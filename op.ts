d177a0408aeedc452ae75553f80a32353297b598
Bilal Shaheen <161133519+BilalShaheen09@users.noreply.github.com>
2024-03-09 12:20:44 -0800

    Add files via upload

---
+++ op.ts
@@ -0,0 +1,346 @@
+const digit1 = 20;
+const digit2 = 10;
+
+console.log(digit1 + digit2);
+
+const digit3 = 34;
+const digit4 = 5;
+
+console.log(digit3 + digit4);
+
+
+const digit9 = 870;
+const digit10 = 434;
+
+console.log(digit9 + digit10);
+
+
+console.log (870 + 760);
+
+
+const twodolllar = 20;
+const onedollar  =  12;
+
+console.log(twodolllar + onedollar);
+
+const fiveyuan = 33;
+const nineyuan = 25;
+
+console.log(fiveyuan + nineyuan);
+
+console.log (33 + 47);
+
+const sixdollar = 77;
+const ninedollar = 55;
+
+console.log (sixdollar + ninedollar);
+
+const room1 = 6;
+const room2 = 9;
+
+console.log(room1 + room2);
+
+
+const pencilinbox1 = 55
+const pencilbox2 = 6
+
+const sum = pencilinbox1 + pencilbox2 + 21
+
+console.log(sum);
+
+const kidsoutside = 7
+const kidsinside = 23
+
+const sum = kidsoutside + kidsinside 
+
+console.log(sum);
+
+
+const kidsinroom = 33
+const kidsoutsdieroom = 40
+
+const sum = kidsinroom + kidsoutsdieroom + 30
+
+console.log(sum);
+
+
+
+
+const rupees80 = 80
+const rupees90 = 67
+
+const sub = rupees80 - rupees90;
+
+console.log (sub);
+
+
+const studentspresent = 134
+const studentabsent = 26
+
+const sub = studentspresent - studentabsent
+
+console.log(sub)
+
+
+const apples1 = 99
+const apple2 = 43
+
+const sub = apples1 - apple2
+
+console.log (sub)
+
+
+const pricemango = 180
+const pricemangopremium = 90
+
+const sub = pricemango - pricemangopremium
+
+console.log(sub)
+
+const amount1 = 500
+const amount2 = 344
+
+const sub = amount1 - amount2
+
+console.log(sub)
+
+//........................end
+
+
+const number2 = 22
+const number1 = 5
+
+ const div = number1 * number2
+
+   console.log(div)
+
+
+   const pakbus = 44
+   const pakbus2 = 22
+
+   const div = pakbus * pakbus2
+
+   console.log(div)
+
+
+   const karachikings = 4
+   const QG = 8
+   const div = karachikings * QG
+
+   console.log(div)
+
+
+   const lahoreqalanders = 7
+   const multansultan = 11
+
+   const div =  lahoreqalanders * multansultan
+
+   console.log(div)
+
+
+   const psl9 = 36
+   const ipl9 = 83
+
+   const div = psl9 * ipl9
+
+   console.log(div)
+
+
+   const bpl9 = 7
+   const bpl10 = 9
+
+   const  div = bpl9 / bpl10
+
+   console.log(div)
+
+
+
+   const bpl2 = 88
+   const bpl3 = 23
+
+   const  div = bpl2 / bpl3
+
+   console.log(div)
+
+//end.......................................
+
+
+const watch = 8
+const smartwatch = 5
+
+const mod = watch % smartwatch
+
+console.log(mod)
+
+
+
+const num1 = 5
+const num2 = 5
+
+const power = num1 * num2
+
+
+console.log (power);
+
+
+const psl7 = 33
+const psl8 = 4 
+
+const power = psl7 * psl8
+
+console.log(power);
+
+
+const pakteam = 90
+const indteam = 45
+
+const power = pakteam * indteam
+
+console.log(power);
+
+
+const kings = 4
+const united = 2
+
+const power = kings * united
+console.log(power)
+
+
+//end....>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
+const karachikings = 9
+const lahore = 4
+
+const div = karachikings / lahore
+console.log(div)
+
+
+const usateam = 23
+const zimteam = 12
+
+const div = usateam / zimteam
+
+console.log(div);
+
+
+const englandteam = 110
+const austeam = 190
+
+const div = englandteam / austeam
+console.log(div);
+
+const china = 20
+const usa = 8
+
+const div = china / usa
+console.log(div)
+
+const india = 200
+const france = 56
+
+const div = india / france
+
+console.log(div);
+
+//end.>>>>>>>>>>>>>>>>>
+
+const firstname = "Shaheen"
+const lastname = "Bilal KK"
+
+console.log (firstname + " " + lastname);
+
+const firstname1 = "Shahid"
+const lastname2 = "Afridi"
+
+console.log(firstname1+lastname2);
+
+const first = "AlexHales"
+const second = "Khan"
+
+console.log(first+second);
+
+console.log(first-second);
+
+console.log(first+ "" +second);
+
+const headcoach = "shanewatson"
+const bowlingcoach = "RossTaylor"
+
+//console.log(headcoach+bowlingcoach);
+
+console.log(headcoach+ "" +bowlingcoach);
+
+const teampk = "kaim"
+const teamind = "khani"
+
+console.log (teampk + " " + teamind);
+
+const ary = "ARY"
+const ary2 = "Digital"
+
+console.log(ary + " " + ary2);
+
+//end>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
+let p = 10
+p = p + 5
+
+console.log(p);
+
+let j = 33
+j = j + 9 +3
+
+console.log(j)
+
+let n = 12
+n = n - 2
+console.log(n);
+
+let x = 24
+x = x - 8 
+console.log(x);
+
+let d = 99
+d = d + 6
+
+console.log(d);
+
+let f = 15 
+f = f - 6
+
+console.log(f);
+
+
+//end >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
+
+//5==2 is false
+
+console.log(10>4);
+
+console.log(3>1);
+
+console.log(56<54);
+
+console.log(23>14);
+
+console.log(9<8);
+
+console.log(7==2);
+
+console.log(8<11);
+
+console.log(33>40);
+
+console.log(12<= 14);
+
+console.log(45>= 29);
+
+console.log(51>= 62); 
+
+
+
+
